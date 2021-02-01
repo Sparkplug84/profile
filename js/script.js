5495483ab14cd6575a135eb75d8d18249aed11d9
Mark McClean <mcclean_mark_02@hotmail.com>
2021-02-01 21:17:01 +0000

    fixed the navbar so it stays above the background image.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
-const navIcon = document.getElementById('nav-icon1')
+const navIcon = document.getElementById('nav-icon')
+const heroBackground = document.querySelector('.hero-header-text')
 
-navIcon.addEventListener('click', () => navIcon.classList.toggle('open'))
+navIcon.addEventListener('click', () => {
+    navIcon.classList.toggle('open')
+    heroBackground.classList.toggle('hero-expand')
+})
+
+
