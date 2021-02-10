dcf2facb63931ccea4e599d532fa288f5ba5b691
Mark McClean <mcclean_mark_02@hotmail.com>
2021-02-10 16:38:58 +0000

    Loading animation and navbar/callout transition added

--- js/script.js
+++ js/script.js
@@ -20,3 +20,28 @@ function checkProfilePhoto() {
             profilePhoto.classList.add('show')
         }
 }
+
+const loadText = document.querySelector('.loading-text')
+const loadImage = document.querySelector('.hero-header-text')
+const callout = document.querySelector('.callout')
+const navbar = document.querySelector('.navbar')
+
+let load = 0
+let int = setInterval(blurring, 30)
+
+function blurring() {
+    load++
+
+    if (load > 99) {
+        clearInterval(int)
+        navbar.classList.add('show')
+        callout.classList.add('show')
+    }
+    loadText.innerHTML = `Profile loading...${load}%`
+    loadText.style.opacity = scale(load, 0, 100, 1, 0)
+    loadImage.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
+}
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
+}
